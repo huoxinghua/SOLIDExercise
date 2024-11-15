51b5e9e62123f26ad396e51b4d8c23052e58892d
huoxinghua <huoxinghuacn@gmail.com>
2024-11-15 09:20:13 -0700

    make the wall and door a little lower

--- Assets/Scripts/Managers/Singleton.cs
+++ Assets/Scripts/Managers/Singleton.cs
@@ -29,7 +29,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         if (_instance == null)
         {
             _instance = this as T;
-            DontDestroyOnLoad(gameObject);  
+           // DontDestroyOnLoad(gameObject);  
         }
         else
         {
